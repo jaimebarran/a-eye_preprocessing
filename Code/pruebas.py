6c42e1884f6870b6704b8c614a836aa8db90e475
jaimebarran <jaimebarran@gmail.com>
2022-08-26 15:06:04 +0200

    update pruebas.py

--- Code/pruebas.py
+++ Code/pruebas.py
@@ -6,13 +6,13 @@ base_dir = '/mnt/sda1/Repos/a-eye/a-eye_preprocessing/ANTs/best_subjects_eye_cc/
 gt_path = base_dir + 'Probability_Maps/prob_map_cropped_th0.nii.gz' # GT
 pr_path = base_dir + 'reg_cropped_other_subjects/sub-01_reg_cropped/labels2template.nii.gz' # Labels' image to compare to GT
 
-# forma 1 --> funciona
+# forma 1 --> works
 # gt_im = nib.load(gt_path)
 # pr_im = nib.load(pr_path)
 # gt_arr = np.array(gt_im.dataobj)
 # pr_arr = np.array(pr_im.dataobj)
 
-# forma 2 --> no funciona
+# forma 2 --> doesn't work --> do GetArrayFromImage() instead
 reader = sitk.ImageFileReader()
 reader.SetFileName(gt_path)
 gt_sitk = sitk.Cast(reader.Execute(), sitk.sitkUInt8)
@@ -23,8 +23,8 @@ pr_sitk = sitk.Cast(reader.Execute(), sitk.sitkUInt8)
 gt_arr = sitk.GetArrayFromImage(gt_sitk)
 pr_arr = sitk.GetArrayFromImage(pr_sitk)
 
-print(f'gt: {np.count_nonzero(gt_arr)}, pr: {np.count_nonzero(pr_arr)}')
 print(f'gt_arr: {gt_arr.shape}, pr_arr: {pr_arr.shape}')
+print(f'gt: {np.count_nonzero(gt_arr==1)}, pr: {np.count_nonzero(pr_arr==1)}')
 
 def dice_norm_metric(ground_truth, predictions):
     '''
@@ -44,19 +44,25 @@ def dice_norm_metric(ground_truth, predictions):
             k = 1.0
         else:
             k = (1 - r) * np.sum(gt) / (r * (len(gt.flatten()) - np.sum(gt)))
+            print(f'np.sum(gt)={np.sum(gt)}, len(gt.flatten())={len(gt.flatten())}')
         tp = np.sum(seg[gt == 1])
-        # print(tp)
+        print(f'tp: {tp}')
         fp = np.sum(seg[gt == 0])
+        print(f'fp: {fp}')
         fn = np.sum(gt[seg == 0])
+        print(f'fn: {fn}')
         fp_scaled = k * fp
+        print(f'fp_scaled: {fp_scaled}')
         dsc_norm = 2 * tp / (fp_scaled + 2 * tp + fn)
+
+        dsc = 2 * tp / (fp + 2 * tp + fn)
 
         fpr = fp / (len(gt.flatten()) - np.sum(gt))
         if np.sum(gt) == 0:
             fnr = 1.0
         else:
             fnr = fn / np.sum(gt)
-        return dsc_norm # fpr, fnr
+        return dsc_norm, dsc # fpr, fnr
 
 gt_mask = gt_arr != 0
 pr_mask = pr_arr != 0
@@ -65,4 +71,4 @@ pr_mask = pr_arr != 0
 # gt_mask = gt_arr==4 or gt_arr==5
 # pr_mask = pr_arr==4 or gt_arr==5
 
-print(f'ndsc: {dice_norm_metric( gt_mask, pr_arr!=0 )}')
+print(f'ndsc vs dsc: {dice_norm_metric( gt_arr==1, pr_arr==1 )}')
