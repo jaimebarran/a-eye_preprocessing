d31996902cfb008890c9b2be0705d71e003ce5ce
jaimebarran <jaimebarran@gmail.com>
2022-09-01 15:48:06 +0200

    antsApplyTransforms labels2subject INVERSE N=5

--- Code/antsEyeRegistrationLOOP.py
+++ Code/antsEyeRegistrationLOOP.py
@@ -5,8 +5,8 @@ base_dir = '/mnt/sda1/Repos/a-eye/a-eye_preprocessing/ANTs/'
 # ref_colin27 = '/mnt/sda1/ANTs/input/colin27/tpl-MNIColin27_T1w.nii.gz'
 # eye_mask_mni = '/mnt/sda1/ANTs/input/mni152/tpl-MNI152NLin2009cAsym_res-01_desc-eye_mask.nii.gz'
 # template_cc = '/mnt/sda1/Repos/a-eye/a-eye_preprocessing/ANTs/best_subjects_eye_cc/CustomTemplate_5_n1/template0.nii.gz'
-template_cc_cropped = '/mnt/sda1/Repos/a-eye/a-eye_preprocessing/ANTs/best_subjects_eye_cc/CustomTemplate_5_n1/template0_cropped_15vox.nii.gz'
-# template_cc_cropped = '/mnt/sda1/Repos/a-eye/a-eye_preprocessing/ANTs/best_subjects_eye_cc/CustomTemplate_1/sub-20_T1_cropped.nii.gz'
+# template_cc_cropped = '/mnt/sda1/Repos/a-eye/a-eye_preprocessing/ANTs/best_subjects_eye_cc/CustomTemplate_5_n1/template0_cropped_15vox.nii.gz'
+template_labels_cropped = '/mnt/sda1/Repos/a-eye/a-eye_preprocessing/ANTs/best_subjects_eye_cc/CustomTemplate_5_n1/Probability_Maps/prob_map_cropped_th0.nii.gz'
 
 # List of best subjects to do the registration
 # best_subjects_cc = ['sub-02','sub-03','sub-20','sub-29','sub-33'] # 5
@@ -100,8 +100,8 @@ for i in range(len(best_subjects_cc)):
 for i in range(len(rest_subjects)):
     # input_t1 = base_dir + 'best_subjects_eye_cc/' + best_subjects_cc[i] + '_T1_aff.nii.gz'
     # input_labels = base_dir + 'a123/' + rest_subjects[i] + '/input/' + rest_subjects[i] + '_labels.nii.gz'
-    # input_t1_cropped = base_dir + 'a123/' + rest_subjects[i] + '/input/' + rest_subjects[i] + '_T1_cropped.nii.gz'
-    input_labels_cropped = base_dir + 'a123/' + rest_subjects[i] + '/input/' + rest_subjects[i] + '_labels_cropped.nii.gz'
+    input_t1_cropped = base_dir + 'a123/' + rest_subjects[i] + '/input/' + rest_subjects[i] + '_T1_cropped.nii.gz'
+    # input_labels_cropped = base_dir + 'a123/' + rest_subjects[i] + '/input/' + rest_subjects[i] + '_labels_cropped.nii.gz'
     output = base_dir +  'best_subjects_eye_cc/CustomTemplate_5_n1/reg_cropped_other_subjects/' # Change this when doing new extractions
     output_reg_cropped_path = output + rest_subjects[i] + '_reg_cropped/'
     if not os.path.exists(output_reg_cropped_path):
@@ -117,28 +117,28 @@ for i in range(len(rest_subjects)):
     # os.system(command1)
 
     # # ApplyTransforms (for cropped images)
-    command2 = 'antsApplyTransforms -d 3 ' + \
-    ' -i ' +  input_labels_cropped + \
-    ' -o ' +  output_reg_cropped_path + 'labels2template2.nii.gz' + \
-    ' -r ' +  template_cc_cropped + \
-    ' -n ' + 'MultiLabel' + \
-    ' -t ' + output_reg_cropped_path + '1Warp.nii.gz' + \
-    ' -t ' + '[' + output_reg_cropped_path + '0GenericAffine.mat, 0 ]' + \
-    ' --float 0 --verbose 1'
-    print(command2)
-    os.system(command2)
-
-    # ApplyTransforms (for cropped images) with inverse transform to get the template labels into subject space
     # command2 = 'antsApplyTransforms -d 3 ' + \
     # ' -i ' +  input_labels_cropped + \
-    # ' -o ' +  output_reg_cropped_path + 'labels2subject.nii.gz' + \
+    # ' -o ' +  output_reg_cropped_path + 'labels2template2.nii.gz' + \
     # ' -r ' +  template_cc_cropped + \
     # ' -n ' + 'MultiLabel' + \
-    # ' -t ' + '[' + output_reg_cropped_path + '0GenericAffine.mat, 1 ]' + \
-    # ' -t ' + output_reg_cropped_path + '1InverseWarp.nii.gz' + \
+    # ' -t ' + output_reg_cropped_path + '1Warp.nii.gz' + \
+    # ' -t ' + '[' + output_reg_cropped_path + '0GenericAffine.mat, 0 ]' + \
     # ' --float 0 --verbose 1'
-    # # print(command2)
+    # print(command2)
     # os.system(command2)
+
+    # ApplyTransforms (for cropped images) with inverse transform to get the template labels into subject space
+    command2 = 'antsApplyTransforms -d 3 ' + \
+    ' -i ' +  template_labels_cropped + \
+    ' -o ' +  output_reg_cropped_path + 'labels2subject.nii.gz' + \
+    ' -r ' +  input_t1_cropped + \
+    ' -n ' + 'MultiLabel' + \
+    ' -t ' + '[' + output_reg_cropped_path + '0GenericAffine.mat, 1 ]' + \
+    ' -t ' + output_reg_cropped_path + '1InverseWarp.nii.gz' + \
+    ' --float 0 --verbose 1'
+    # print(command2)
+    os.system(command2)
 
     # Dealing with files in that folder
     # for f in glob.glob(output_reg_cropped_path + 'labels2subject.nii.gz'):
